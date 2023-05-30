3c358005caf515e3ff89e9a9ea009586cc6541fc
Gillian <105639251+MaronillaG@users.noreply.github.com>
2023-05-30 11:24:19 +0200

    added gitignore

---
+++ data.js
@@ -0,0 +1,39 @@
+export let movieData = {
+    "The Darjeeling Limited": {
+      plot: "A year after their father's funeral, three brothers travel across India by train in an attempt to bond with each other.",
+      cast: ["Jason Schwartzman", "Owen Wilson", "Adrien Brody"],
+      runtime: 151,
+      rating: 7.2,
+      year: 2007,
+      img: "https://raw.githubusercontent.com/maronillaG/filmFinder/Assets/Darjeeling_Limited_Poster.jpg",
+    },
+    "The Royal Tenenbaums": {
+      plot: "The eccentric members of a dysfunctional family reluctantly gather under the same roof for various reasons",
+      rating: 7.6,
+      year: 2001,
+      cast: ["Gene Hackman", "Gwnyeth Paltrow", "Anjelica Huston"],
+      runtime: 170,
+      img: "https://raw.githubusercontent.com/maronillaG/filmFinder/Assets/The_Royal_Tenenbaums_Poster.webp",
+    },
+    "Fantastic Mr. Fox": {
+      year: 2009,
+      plot: "An urbane fox cannot resist returning to his farm raiding ways and then must help his community survive the farmers' retaliation.",
+      cast: [
+        "George Clooney",
+        "Meryl Streep",
+        "Bill Murray",
+        "Jason Schwartzman",
+      ],
+      runtime: 147,
+      rating: 7.9,
+      img: "https://raw.githubusercontent.com/maronillaG/filmFinder/Assets/fantastic_fox.jpeg",
+    },
+    "The Grand Budapest Hotel": {
+      rating: 8.1,
+      runtime: 159,
+      year: 2014,
+      plot: "A writer encounters the owner of an aging high-class hotel, who tells him of his early years serving as a lobby boy in the hotel's glorious years under an exceptional concierge.",
+      cast: ["Ralph Fiennes", "F. Murray Abraham", "Mathieu Amalric"],
+      img: "https://raw.githubusercontent.com/maronillaG/filmFinder/Assets/Grand_Budapest_Hotel.webp",
+    },
+  };
