04a4db56333268ba270764cef07290e813d25e59
Gillian <105639251+MaronillaG@users.noreply.github.com>
2023-05-30 14:31:09 +0200

    first draft logic to access object properties

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-import { movieData } from ' ./data.js';
+import { movieData } from './data.js';
+
+// access the object via the key: 
+movieData['The Grand Budapest Hotel'].plot;
+
+// go through object using for-in loop
+for (let title in movieData)
+    console.log(title, movieData[title].year);
+
+// check existence of title:
+let searchTerm = 'X men' // this needs to be
+
+   if ( searchTerm in movieData ) {
+    console.log('Yes');
+   } else { console.log("not found")};
--- data.js
+++ data.js
@@ -5,7 +5,7 @@ export let movieData = {
       runtime: 151,
       rating: 7.2,
       year: 2007,
-      img: "https://raw.githubusercontent.com/maronillaG/filmFinder/Assets/Darjeeling_Limited_Poster.jpg",
+      img: "https://raw.githubusercontent.com/MaronillaG/FilmFinder/main/Assets/Darjeeling_Limited_Poster.jpg",
     },
     "The Royal Tenenbaums": {
       plot: "The eccentric members of a dysfunctional family reluctantly gather under the same roof for various reasons",
@@ -13,7 +13,7 @@ export let movieData = {
       year: 2001,
       cast: ["Gene Hackman", "Gwnyeth Paltrow", "Anjelica Huston"],
       runtime: 170,
-      img: "https://raw.githubusercontent.com/maronillaG/filmFinder/Assets/The_Royal_Tenenbaums_Poster.webp",
+      img: "https://raw.githubusercontent.com/MaronillaG/FilmFinder/main/Assets/The_Royal_Tenenbaums_Poster.webp",
     },
     "Fantastic Mr. Fox": {
       year: 2009,
@@ -26,7 +26,7 @@ export let movieData = {
       ],
       runtime: 147,
       rating: 7.9,
-      img: "https://raw.githubusercontent.com/maronillaG/filmFinder/Assets/fantastic_fox.jpeg",
+      img: "https://raw.githubusercontent.com/maronillaG/filmFinder/main/Assets/fantastic_fox.jpeg",
     },
     "The Grand Budapest Hotel": {
       rating: 8.1,
@@ -34,6 +34,6 @@ export let movieData = {
       year: 2014,
       plot: "A writer encounters the owner of an aging high-class hotel, who tells him of his early years serving as a lobby boy in the hotel's glorious years under an exceptional concierge.",
       cast: ["Ralph Fiennes", "F. Murray Abraham", "Mathieu Amalric"],
-      img: "https://raw.githubusercontent.com/maronillaG/filmFinder/Assets/Grand_Budapest_Hotel.webp",
+      img: "https://raw.githubusercontent.com/maronillaG/filmFinder/main/Assets/Grand_Budapest_Hotel.webp",
     },
   };
